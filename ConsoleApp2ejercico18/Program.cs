1e04431db14a2556be66f08e71c9c7279cc5fc35
pamela <pamela@AIWA_NA-1413>
2022-07-04 18:40:57 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp2ejercico18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2ejercico18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("ingrese el numero");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0)
+                Console.WriteLine("el numero es positivo");
+            else
+                Console.WriteLine("el numero es negativo");
+            Console.ReadLine();
+
+        }
+    }
+}
